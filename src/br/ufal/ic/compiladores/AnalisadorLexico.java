5e38946b273fe7f904d8599d8f83573bc13695e4
Dalton <f.dalton.bd@gmail.com>
2013-04-13 08:20:28 -0300

    Pequenas modificacoes na tabela interna e no analisador lexico

--- src/br/ufal/ic/compiladores/AnalisadorLexico.java
+++ src/br/ufal/ic/compiladores/AnalisadorLexico.java
@@ -15,160 +15,169 @@ import br.ufal.ic.compiladores.token.Token;
  */
 public class AnalisadorLexico {
 
-    private static int posLinha = 1;
-    private static int posColuna = 1;
-    private static BufferedReader buffer;
-    private static char charBuff;
-    private static final char EOLR = '\r';
-    private static final char EOLN = '\n';
-    private static final char EOF = (char) -1;
+	private static int posLinha = 1;
+	private static int posColuna = 1;
+	private static BufferedReader buffer;
+	private static char charBuff;
+	private static final char EOLR = '\r';
+	private static final char EOLN = '\n';
+	private static final char EOF = (char) -1;
 
-    /* delimitar os caracteres aceitos pela linguagem */
-    private static final int ASCII_INI = 32;
-    private static final int ASCII_FIM = 126;
+	/* delimitar os caracteres aceitos pela linguagem */
+	private static final int ASCII_INI = 32;
+	private static final int ASCII_FIM = 126;
 
-    public static void setBuffer(BufferedReader buffer) throws IOException {
-	AnalisadorLexico.buffer = buffer;
-	charBuff = (char) buffer.read();
-    }
-
-    /**
-     * le o proximo caracter e incrementa o contador de coluna
-     * 
-     * @throws IOException
-     */
-    private static void lerBuffer() {
-	posColuna++;
-	try {
-	    charBuff = (char) buffer.read();
-	} catch (IOException e) {
-	    System.err.println("Erro na leitura do arquivo!");
-	    e.printStackTrace();
+	public static void setBuffer(BufferedReader buffer) throws IOException {
+		AnalisadorLexico.buffer = buffer;
+		charBuff = (char) buffer.read();
 	}
-    }
 
-    /**
-     * 
-     * @return Token
-     * @throws UndefinedTokenException
-     */
-    public static Token nextToken() throws UndefinedTokenException {
-
-	int posXInit;
-	int posYInit;
-	ClasseToken classe = null;
-	String lexema = "";
-
-	// percorre ate encontrar algo diferente de espaco ou enter ou chegar ao
-	// final do arquivo.
-	while (charBuff == EOLR || charBuff == EOLN
-		|| Character.isWhitespace(charBuff) || charBuff == EOF) {
-	    // Windows = \r\n, mac = \r, unix = \n
-	    if (charBuff == EOLR) {
-		posColuna = 0;
-		posLinha++;
-		lerBuffer();
-		if (charBuff == EOLN) {
-		    posColuna = 0;
-		    lerBuffer();
+	/**
+	 * le o proximo caracter e incrementa o contador de coluna
+	 * 
+	 * @throws IOException
+	 */
+	private static void lerBuffer() {
+		posColuna++;
+		try {
+			charBuff = (char) buffer.read();
+		} catch (IOException e) {
+			System.err.println("Erro na leitura do arquivo!");
+			e.printStackTrace();
 		}
-	    } else if (charBuff == EOLN) {
-		posColuna = 0;
-		posLinha++;
-		lerBuffer();
-	    } else if (charBuff == EOF) {
-		return null;
-	    } else {
-		lerBuffer();
-	    }
+	}
+	/**
+	 * remove os espacos em branco, enter e outros.
+	 * funciona apenas sobre demanda, nao limpa o arquivo inteiro.
+	 */
+	public static boolean clearWhiteSpace(){
+		while (charBuff == EOLR || charBuff == EOLN
+				|| Character.isWhitespace(charBuff) || charBuff == EOF) {
+			// Windows = \r\n, mac = \r, unix = \n
+			if (charBuff == EOLR) {
+				posColuna = 0;
+				posLinha++;
+				lerBuffer();
+				if (charBuff == EOLN) {
+					posColuna = 0;
+					lerBuffer();
+				}
+			} else if (charBuff == EOLN) {
+				posColuna = 0;
+				posLinha++;
+				lerBuffer();
+			} else if (charBuff == EOF) {
+				return false;
+			} else {
+				lerBuffer();
+			}
+		}
+		return true;
 	}
 
-	// posicao do token
-	posXInit = posColuna;
-	posYInit = posLinha;
+	/**
+	 * 
+	 * @return Token
+	 * @throws UndefinedTokenException
+	 */
+	public static Token nextToken() throws UndefinedTokenException {
 
-	if (Character.isLetter(charBuff)) {
-	    while (Character.isLetterOrDigit(charBuff)) {
-		lexema += charBuff;
-		lerBuffer();
-	    }
+		int posXInit;
+		int posYInit;
+		ClasseToken classe = null;
+		String lexema = "";
 
-	    if (lexema.equals("else") && Character.isWhitespace(charBuff)) {
-		lerBuffer();
-		if (charBuff == 'i') {
-		    lexema += ' ';
-		    while (Character.isLetterOrDigit(charBuff)) {
+		// percorre ate encontrar algo diferente de espaco ou enter ou chegar ao
+		// final do arquivo.
+		if(!clearWhiteSpace()) return null;
+
+
+		// posicao do token
+		posXInit = posColuna;
+		posYInit = posLinha;
+
+		if (Character.isLetter(charBuff) || charBuff == '_') {
+			while (Character.isLetterOrDigit(charBuff)) {
+				lexema += charBuff;
+				lerBuffer();
+			}
+
+			if (lexema.equals("else")){
+				if(!clearWhiteSpace()) return null;
+				if (charBuff == 'i') {
+					lexema += ' ';
+					while (Character.isLetterOrDigit(charBuff)) {
+						lexema += charBuff;
+						lerBuffer();
+					}
+				}
+			}
+
+			if (TabelaInterna.contains(lexema)) {
+				classe = TabelaInterna.getTokenClass(lexema.toLowerCase());
+			} else {
+				classe = ClasseToken.ID;
+			}
+		} else if (Character.isDigit(charBuff)) {
+			int quantPonto = 0;
+			while (Character.isDigit(charBuff) || charBuff == '.') {
+				if (charBuff == '.') {
+					if (++quantPonto > 1) {
+						throw new UndefinedTokenException(
+								String.valueOf(charBuff), posLinha, posColuna);
+					}
+				}
+				lexema += charBuff;
+				lerBuffer();
+			}
+			classe = ClasseToken.CONST_NUM;
+		} else if (charBuff == '"') {
+			// captura cadeia de caracteres
 			lexema += charBuff;
 			lerBuffer();
-		    }
-		}
-	    }
+			while (charBuff >= ASCII_INI && charBuff <= ASCII_FIM) {
+				if (charBuff == '"') {
+					lexema += charBuff;
+					classe = ClasseToken.CADEIA_CARACTER;
+					lerBuffer();
+					break;
+				}
+				lexema += charBuff;
+				lerBuffer();
+			}
+		} else if (charBuff == '\'') {
+			// captura char
+			lexema += charBuff;
+			lerBuffer();
+			if (Character.isLetter(charBuff)) {
+				lexema += charBuff;
+				lerBuffer();
+				if (charBuff == '\'') {
+					lexema += charBuff;
+					classe = ClasseToken.CARACTER;
+					lerBuffer();
+				} else {
+					throw new UndefinedTokenException(lexema, posLinha,
+							posColuna);
+				}
 
-	    if (TabelaInterna.contains(lexema)) {
-		classe = TabelaInterna.getTokenClass(lexema.toLowerCase());
-	    } else {
-		classe = ClasseToken.ID;
-	    }
-	} else if (Character.isDigit(charBuff)) {
-	    int quantPonto = 0;
-	    while (Character.isDigit(charBuff) || charBuff == '.') {
-		if (charBuff == '.') {
-		    if (++quantPonto > 1) {
-			throw new UndefinedTokenException(
-				String.valueOf(charBuff), posLinha, posColuna);
-		    }
-		}
-		lexema += charBuff;
-		lerBuffer();
-	    }
-	    classe = ClasseToken.CONST_NUM;
-	} else if (charBuff == '"') {
-	    // captura cadeia de caracteres
-	    lexema += charBuff;
-	    lerBuffer();
-	    while (charBuff >= ASCII_INI && charBuff <= ASCII_FIM) {
-		if (charBuff == '"') {
-		    lexema += charBuff;
-		    classe = ClasseToken.CADEIA_CARACTER;
-		    lerBuffer();
-		    break;
-		}
-		lexema += charBuff;
-		lerBuffer();
-	    }
-	} else if (charBuff == '\'') {
-	    // captura char
-	    lexema += charBuff;
-	    lerBuffer();
-	    if (Character.isLetter(charBuff)) {
-		lexema += charBuff;
-		lerBuffer();
-		if (charBuff == '\'') {
-		    lexema += charBuff;
-		    classe = ClasseToken.CARACTER;
-		    lerBuffer();
+			}
 		} else {
-		    throw new UndefinedTokenException(lexema, posLinha,
-			    posColuna);
+			// captura operadores e delimitadores e outras coisas
+			lexema += charBuff;
+			lerBuffer();
+			if (TabelaInterna.contains(lexema + charBuff)) {
+				lexema += charBuff;
+				lerBuffer();
+			}
+			classe = TabelaInterna.getTokenClass(lexema);
 		}
 
-	    }
-	} else {
-	    // captura operadores e delimitadores e outras coisas
-	    lexema += charBuff;
-	    lerBuffer();
-	    if (TabelaInterna.contains(lexema + charBuff)) {
-		lexema += charBuff;
-		lerBuffer();
-	    }
-	    classe = TabelaInterna.getTokenClass(lexema);
-	}
+		if (classe == null) {
+			// captura coisas q nao foram classificadas
+			throw new UndefinedTokenException(lexema, posXInit, posYInit);
+		}
 
-	if (classe == null) {
-	    // captura coisas q nao foram classificadas
-	    throw new UndefinedTokenException(lexema, posXInit, posYInit);
+		return new Token(classe, lexema, posYInit, posXInit);
 	}
-
-	return new Token(classe, lexema, posYInit, posXInit);
-    }
 }
--- src/br/ufal/ic/compiladores/tabela/TabelaInterna.java
+++ src/br/ufal/ic/compiladores/tabela/TabelaInterna.java
@@ -6,83 +6,86 @@ import br.ufal.ic.compiladores.token.ClasseToken;
 
 public class TabelaInterna {
 
-    private final static HashMap<String, ClasseToken> tabelaInterna = new HashMap<String, ClasseToken>();
+	private final static HashMap<String, ClasseToken> tabelaInterna = new HashMap<String, ClasseToken>();
 
-    static {
+	static {
 
-	// tipos
-	tabelaInterna.put("int", ClasseToken.TIPO);
-	tabelaInterna.put("float", ClasseToken.TIPO);
-	tabelaInterna.put("double", ClasseToken.TIPO);
-	tabelaInterna.put("char", ClasseToken.TIPO);
+		// tipos
+		tabelaInterna.put("int", ClasseToken.TIPO);
+		tabelaInterna.put("float", ClasseToken.TIPO);
+		tabelaInterna.put("double", ClasseToken.TIPO);
+		tabelaInterna.put("char", ClasseToken.TIPO);
 
-	// bool literais
-	tabelaInterna.put("false", ClasseToken.BOOLEAN);
-	tabelaInterna.put("true", ClasseToken.BOOLEAN);
+		// bool literais
+		tabelaInterna.put("false", ClasseToken.BOOLEAN);
+		tabelaInterna.put("true", ClasseToken.BOOLEAN);
 
-	// metodos proprios
-	tabelaInterna.put("main", ClasseToken.FUNC_MAIN);
-	tabelaInterna.put("printf", ClasseToken.FUNC_PRINTF);
-	tabelaInterna.put("scanf", ClasseToken.FUNC_SCANF);
+		// metodos proprios
+		tabelaInterna.put("main", ClasseToken.FUNC_MAIN);
+		tabelaInterna.put("printf", ClasseToken.FUNC_PRINTF);
+		tabelaInterna.put("scanf", ClasseToken.FUNC_SCANF);
 
-	// palavras chave
-	tabelaInterna.put("void", ClasseToken.VOID);
-	tabelaInterna.put("if", ClasseToken.IF);
-	tabelaInterna.put("else", ClasseToken.ELSE);
-	tabelaInterna.put("else if", ClasseToken.ELSE_IF);
-	tabelaInterna.put("for", ClasseToken.FOR);
-	tabelaInterna.put("return", ClasseToken.RETURN);
-	tabelaInterna.put("while", ClasseToken.WHILE);
-	tabelaInterna.put("{", ClasseToken.ACHAVE);
-	tabelaInterna.put("}", ClasseToken.FCHAVE);
+		// palavras chave
+		tabelaInterna.put("void", ClasseToken.VOID);
+		tabelaInterna.put("if", ClasseToken.IF);
+		tabelaInterna.put("else", ClasseToken.ELSE);
+		tabelaInterna.put("else if", ClasseToken.ELSE_IF);
+		tabelaInterna.put("for", ClasseToken.FOR);
+		tabelaInterna.put("return", ClasseToken.RETURN);
+		tabelaInterna.put("while", ClasseToken.WHILE);
 
-	// separadores
-	tabelaInterna.put("[", ClasseToken.ACOLCHETE);
-	tabelaInterna.put("]", ClasseToken.FCOLCHETE);
-	tabelaInterna.put("(", ClasseToken.APARENTESE);
-	tabelaInterna.put(")", ClasseToken.FPARENTESE);
-	tabelaInterna.put(",", ClasseToken.VIRGULA);
-	tabelaInterna.put(".", ClasseToken.PONTO);
-	tabelaInterna.put(";", ClasseToken.PONTO_VIRGULA);
+		// separadores
+		tabelaInterna.put("{", ClasseToken.ACHAVE);
+		tabelaInterna.put("}", ClasseToken.FCHAVE);
+		tabelaInterna.put("[", ClasseToken.ACOLCHETE);
+		tabelaInterna.put("]", ClasseToken.FCOLCHETE);
+		tabelaInterna.put("(", ClasseToken.APARENTESE);
+		tabelaInterna.put(")", ClasseToken.FPARENTESE);
+		tabelaInterna.put(",", ClasseToken.VIRGULA);
+		tabelaInterna.put(".", ClasseToken.PONTO);
+		tabelaInterna.put(";", ClasseToken.PONTO_VIRGULA);
 
-	// atribuicao
-	tabelaInterna.put("=", ClasseToken.ATRIBUICAO);
+		// atribuicao
+		tabelaInterna.put("=", ClasseToken.ATRIBUICAO);
 
-	// negacao
-	tabelaInterna.put("!", ClasseToken.OP_NEGACAO);
+		// negacao
+		tabelaInterna.put("!", ClasseToken.OP_NEGACAO);
 
-	// pre-processador
-	tabelaInterna.put("#", ClasseToken.PREPROCESSADOR);
+		// pre-processador
+		tabelaInterna.put("#", ClasseToken.PREPROCESSADOR);
 
-	// operadores relacionais
-	tabelaInterna.put("==", ClasseToken.OP_IGUAL_IGUAL);
-	tabelaInterna.put("!=", ClasseToken.OP_DIFERENTE);
-	tabelaInterna.put(">", ClasseToken.OP_MAIOR);
-	tabelaInterna.put("<", ClasseToken.OP_MENOR);
-	tabelaInterna.put(">=", ClasseToken.OP_MAIOR_IGUAL);
-	tabelaInterna.put("<=", ClasseToken.OP_MENOR_IGUAL);
+		// operadores relacionais
+		tabelaInterna.put("==", ClasseToken.OP_IGUAL_IGUAL);
+		tabelaInterna.put("!=", ClasseToken.OP_DIFERENTE);
+		tabelaInterna.put(">", ClasseToken.OP_MAIOR);
+		tabelaInterna.put("<", ClasseToken.OP_MENOR);
+		tabelaInterna.put(">=", ClasseToken.OP_MAIOR_IGUAL);
+		tabelaInterna.put("<=", ClasseToken.OP_MENOR_IGUAL);
 
-	// operadores logicos
-	tabelaInterna.put("||", ClasseToken.OP_OU);
-	tabelaInterna.put("&&", ClasseToken.OP_E);
+		// operadores logicos
+		tabelaInterna.put("||", ClasseToken.OP_OU);
+		tabelaInterna.put("&&", ClasseToken.OP_E);
 
-	tabelaInterna.put("&", ClasseToken.OP_E_BINARIO);
+		tabelaInterna.put("&", ClasseToken.OP_E_BINARIO);
+		tabelaInterna.put("|", ClasseToken.OP_OU_BINARIO);
 
-	// operadores aritimeticos
-	tabelaInterna.put("+", ClasseToken.OP_ADICAO);
-	tabelaInterna.put("-", ClasseToken.OP_SUBTRACAO);
-	tabelaInterna.put("/", ClasseToken.OP_DIVISAO);
-	tabelaInterna.put("*", ClasseToken.OP_MULTIPLICACAO);
-	tabelaInterna.put("%", ClasseToken.OP_RESTO);
+		// operadores aritimeticos
+		tabelaInterna.put("+", ClasseToken.OP_ADICAO);
+		tabelaInterna.put("++", ClasseToken.OP_ADICAO_ADICAO);
+		tabelaInterna.put("-", ClasseToken.OP_SUBTRACAO);
+		tabelaInterna.put("--", ClasseToken.OP_SUBTRACAO_SUBTRACAO);
+		tabelaInterna.put("/", ClasseToken.OP_DIVISAO);
+		tabelaInterna.put("*", ClasseToken.OP_MULTIPLICACAO);
+		tabelaInterna.put("%", ClasseToken.OP_RESTO);
 
-    }
+	}
 
-    public static boolean contains(String lexema) {
-	return tabelaInterna.containsKey(String.valueOf(lexema));
-    }
+	public static boolean contains(String lexema) {
+		return tabelaInterna.containsKey(String.valueOf(lexema));
+	}
 
-    public static ClasseToken getTokenClass(String lexema) {
-	ClasseToken classe = tabelaInterna.get(lexema);
-	return classe;
-    }
+	public static ClasseToken getTokenClass(String lexema) {
+		ClasseToken classe = tabelaInterna.get(lexema);
+		return classe;
+	}
 }
